1bb6c1980d5bdca8b7c80c292d66f0f9cde51be3
Diogo Rosa <diogodr@outlook.com>
2021-01-18 17:23:05 -0300

    Finalizaçao do projeto

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+import Routes from 'react';
+
+const routes: React.FC = () => (
+  <Switch>
+    <Route exact path="/" component={Dashboard}/>
+    <Route path="/repositories/:repository+" component={Repository}/>
+  </Switch>
+)
+
+export default routes;
